9235b55ba7e5e10b32eb6abc89b5455bf656387c
Hasan uz zaman Sheikh <hasan@live.in>
2021-03-24 07:44:36 -0700

    Update runpython.sh

--- runpython.sh
+++ runpython.sh
@@ -8,5 +8,6 @@ sudo apt-get upgrade --yes
 sudo apt-get install python3-pip --yes
 echo working directory $(pwd) >> $LOGFILE
 pip3 install -r requirement.txt 
+tar -xzf inputfiles.tar.gz
 python3 filetorun.py  $1 $2 > /tmp/output$1-$2.txt
 echo finished execution $(date) >> $LOGFILE

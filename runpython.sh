c488cad70d0b0bd8494d634736d0926420744ba6
Hasan uz zaman Sheikh <hasan@live.in>
2021-03-24 07:26:00 -0700

    Update runpython.sh

--- runpython.sh
+++ runpython.sh
@@ -3,8 +3,10 @@ LOGFILE=/tmp/mylog$RANDOM.txt
 
 echo starting execution  $(date) > $LOGFILE
 echo machine id $1 total machines $2  $(date) >> $LOGFILE
-echo $(ls -l) >> $LOGFILE
-echo $(pwd) >> $LOGFILE
+sudo apt-get update
+sudo apt-get upgrade --yes
+sudo apt-get install python3-pip --yes
+echo working directory $(pwd) >> $LOGFILE
 pip3 install -r requirement.txt 
 python3 filetorun.py  $1 $2 > /tmp/output$1-$2.txt
 echo finished execution $(date) >> $LOGFILE

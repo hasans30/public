473d618c22ca3fd96468da589e373ba7fed7b918
root <root@MININT-5DTDR2R.redmond.corp.microsoft.com>
2021-03-23 22:47:20 -0700

    run command

--- runpython.sh
+++ runpython.sh
@@ -3,5 +3,5 @@ LOGFILE=/tmp/mylog$RANDOM.txt
 echo starting execution  $(date) > $LOGFILE
 echo machine id $1 total machines $2  $(date) > $LOGFILE
 pip3 install -r requirement.txt 
-python3 filetorun.py  $1 $2
+python3 filetorun.py  $1 $2 > /tmp/output$1-$2.txt
 echo finished execution $(date) >> $LOGFILE

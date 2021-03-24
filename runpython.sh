b092a51ebbd90f9f5caa8e9f3f362e87cbe75b62
root <root@localhost>
2021-03-23 23:08:00 -0700

    some fix

--- runpython.sh
+++ runpython.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 LOGFILE=/tmp/mylog$RANDOM.txt
+
 echo starting execution  $(date) > $LOGFILE
-echo machine id $1 total machines $2  $(date) > $LOGFILE
+echo machine id $1 total machines $2  $(date) >> $LOGFILE
+echo $(ls -l) >> $LOGFILE
+echo $(pwd) >> $LOGFILE
 pip3 install -r requirement.txt 
 python3 filetorun.py  $1 $2 > /tmp/output$1-$2.txt
 echo finished execution $(date) >> $LOGFILE

2fff6530b9486ae285a9a9a684271d0856d2626f
Hasan uz zaman Sheikh <hasan@live.in>
2021-03-23 21:21:26 -0700

    Update runpython.sh

--- runpython.sh
+++ runpython.sh
@@ -2,8 +2,6 @@
 LOGFILE=/tmp/mylog$RANDOM.txt
 echo starting execution  $(date) > $LOGFILE
 echo machine id $1 total machines $2  $(date) > $LOGFILE
-curl $REQUIREMENT_URL -o requirement.txt
 pip3 install -r requirement.txt 
-curl $CODE_URL -o filetorun.py
 python3 filetorun.py  $1 $2
 echo finished execution $(date) >> $LOGFILE
